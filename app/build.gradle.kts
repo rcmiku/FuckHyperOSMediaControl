a603ed1919347272f63024d98886aca40ca2cd47
rcmiku <rcmiku@gmail.com>
2025-01-09 14:00:17 +0800

    Bump version to 1.2.0
    Signed-off-by: rcmiku <rcmiku@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = namespace
         minSdk = 34
         targetSdk = 35
-        versionCode = 1100
-        versionName = "1.1.0"
+        versionCode = 1200
+        versionName = "1.2.0"
     }
     signingConfigs {
         register("release") {
